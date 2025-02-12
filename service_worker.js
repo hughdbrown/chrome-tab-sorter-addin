f81be33f2f931fa7c3373cd7a421229486e18416
Hugh Brown <hughdbrown@yahoo.com>
2025-02-11 17:09:02 -0700

    Add timing and optimizations

--- service_worker.js
+++ service_worker.js
@@ -1,9 +1,10 @@
 function sorter(tabs) {
+    const startTime = performance.now();
     var compare = function(arg1, arg2) {
         if (arg1 == arg2) { return 0; }
         else { return arg1 > arg2 ? 1 : -1; }
     };
-    var normalize = function(url) {
+    var _normalize = function(url) {
         var x = url.replace("https://", "").replace("http://", "").toUpperCase();
         var i = x.indexOf('/');
         var s = x.substring(0, i - 1);
@@ -13,33 +14,65 @@ function sorter(tabs) {
         var tla = parts[parts.length - 1];
         return company + "." + tla + t ;
     };
+
+    var normalized = {};
+    var normalize = function(url) {
+        var n = normalized[url];
+        if (n === undefined) {
+             n = _normalize(url);
+             normalized[url] = n;
+        }
+        return n;
+    };
+
     var sorted_tabs = tabs.sort(function (tab1, tab2) {
         var u1 = normalize(tab1.url);
         var u2 = normalize(tab2.url);
         return compare(u1, u2);
     });
+
+    const elapsed = Math.floor(1000 * (performance.now() - startTime));
+    const length = Object.keys(normalized).length;
+    console.log(`Sorter: ${elapsed} ms / ${length} urls`);
+
     return sorted_tabs;
 }
 
 function unique(tabs) {
+    const startTime = performance.now();
+
     // Get unique tabs by url in a dict.
-    var unique_tabs = {};
+    // var unique_tabs = {};
+    var unique_tabs = new Set();
     var removes = [];
     var arr = [];
     for (const [i, tab] of tabs.entries()) {
-        if (unique_tabs[tab.url] === undefined) {
-            // Tab url not present -- add it.
-            unique_tabs[tab.url] = tab;
-            arr.push(tab);
-	}
-	else {
+        //if (unique_tabs[tab.url] === undefined) {
+        //    // Tab url not present -- add it.
+        //    unique_tabs[tab.url] = tab;
+        //    arr.push(tab);
+	//}
+        if (unique_tabs.has(tab.url)) {
             // Add tab to list to be removed.
             removes.push(tab.id);
+        }
+	else {
+            unique_tabs.add(tab.url);
+            arr.push(tab);
 	}
     }
+    const elapsed = Math.floor(1000 * (performance.now() - startTime));
+    const length = unique_tabs.size;
+    console.log(`Find unique: ${elapsed} ms / ${length} urls`);
 
     if (removes.length > 0) {
+        const startTime = performance.now();
+        const length = removes.length;
+
         chrome.tabs.remove(removes);
+
+        const elapsed = Math.floor(1000 * (performance.now() - startTime));
+        console.log(`Remove unique: ${elapsed} ms / ${length} urls`);
     }
 
     // Return sorted unique tabs.
